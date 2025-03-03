4068bbf3580304de823f2a3a84a6336befefd0e9
oussamaef06 <oussamaelfarihi@gmail.com>
2025-03-03 11:28:36 +0000

    the game work correctly

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1
-var randomNumber2 = Math.floor(Math.random() * 6) + 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var diceImage1 = document.querySelector('.img1');
 var diceImage2 = document.querySelector('.img2');
 
 diceImage1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
-diceImage2.setAttribute("src", "images/dice" + randomNumber1 + ".png");
+diceImage2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "🎲 It's a Draw!";
+}
